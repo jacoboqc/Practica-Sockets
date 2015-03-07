4e9b69004998a6aaf8e0b4a58b4bc035a7c26903
Jacobo Quintáns <jacoboqc@yahoo.es>
2015-03-07 17:40:27 +0100

    Created basic socket

--- src/cliente/Cliente.java
+++ src/cliente/Cliente.java
@@ -1,10 +1,25 @@
 package cliente;
 
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 public class Cliente {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		final String host="localhost";
+		final int puerto=500;
+		Socket sc;
+		DataInputStream entrada;
+		
+		try {
+			sc=new Socket(host, puerto);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 }
