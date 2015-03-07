698fbf6c0e81fc649bb7bd731c63c100745ab575
Jacobo Quintáns <jacoboqc@yahoo.es>
2015-03-07 21:04:53 +0100

    Stops server when pressing <C>

--- src/cliente/Cliente.java
+++ src/cliente/Cliente.java
@@ -1,30 +1,36 @@
 package cliente;
 
+import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class Cliente {
 
 	public static void main(String[] args) {
-		final String host="localhost";
-		final int puerto=500;
-		boolean corriendo=true;
+		final String host = "localhost";
+		final int puerto = 500;
+		boolean corriendo = true;
+		boolean cerrar = false;
 		Socket socket;
 		DataInputStream entrada;
-		
+		BufferedReader teclado = new BufferedReader (new InputStreamReader(System.in));
+
 		try {
-			socket=new Socket(host, puerto);
+			socket = new Socket(host, puerto);
 			System.out.println("Conexión establecida correctamente");
-			entrada=new DataInputStream(socket.getInputStream());
-			while(corriendo){
+			entrada = new DataInputStream(socket.getInputStream());
+			while (corriendo && !cerrar) {
 				System.out.println(entrada.readUTF());
+				if(teclado.readLine().equalsIgnoreCase("C")){
+					cerrar=true;
+				}
 			}
 			socket.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-	}
 
+	}
 }
