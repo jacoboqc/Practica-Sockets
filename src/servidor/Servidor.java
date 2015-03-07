664eba4f15dec08811dc77ab0b7e0b23ea42ac42
Jacobo Quintáns <jacoboqc@yahoo.es>
2015-03-07 17:59:38 +0100

    Server senda data and closes connection

--- src/servidor/Servidor.java
+++ src/servidor/Servidor.java
@@ -4,6 +4,8 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Servidor {
 
@@ -20,10 +22,14 @@ public class Servidor {
 			System.out.println("Esperando conexión...");
 			socket_conexion=socket_servidor.accept();
 			System.out.println("Conexión establecida");
-			
-
+			salida = new DataOutputStream(socket_conexion.getOutputStream());
+			Date fecha = new Date();
+			SimpleDateFormat formato_fecha = new SimpleDateFormat("HH-mm-ss");
+			String hora=formato_fecha.format(fecha.toString());
+			salida.writeUTF(hora);
+			socket_conexion.close();
+			socket_servidor.close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
