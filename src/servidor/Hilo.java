21039bc89e8efce5fdd718b66a17ca6988e6d89a
Jacobo Quintáns <jacoboqc@yahoo.es>
2015-03-19 20:11:33 +0100

    Connection runs in its own thread

---
+++ src/servidor/Hilo.java
@@ -0,0 +1,38 @@
+package servidor;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Hilo extends Thread{
+
+	Socket socket_conexion;
+	DataOutputStream salida;
+	boolean corriendo=true;
+
+	public Hilo(Socket socket_conexion) {
+		this.socket_conexion=socket_conexion;
+	}
+
+	public void run(){
+		try {
+			System.out.println("Conexión establecida");
+			salida = new DataOutputStream(socket_conexion.getOutputStream());
+			while(corriendo){
+				Date fecha = new Date();
+				SimpleDateFormat formato_fecha = new SimpleDateFormat("HH:mm:ss");
+				String hora=formato_fecha.format(fecha);
+				salida.writeUTF(hora);
+				Thread.sleep(1000);
+			}
+			socket_conexion.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
--- src/servidor/Servidor.java
+++ src/servidor/Servidor.java
@@ -1,11 +1,8 @@
 package servidor;
 
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class Servidor {
 
@@ -13,8 +10,7 @@ public class Servidor {
 		final int puerto=500;
 		ServerSocket socket_servidor;
 		Socket socket_conexion;
-		DataOutputStream salida;
-		boolean corriendo=true;
+
 		
 		
 		try {
@@ -22,16 +18,8 @@ public class Servidor {
 			socket_conexion=new Socket();
 			System.out.println("Esperando conexión...");
 			socket_conexion=socket_servidor.accept();
-			System.out.println("Conexión establecida");
-			salida = new DataOutputStream(socket_conexion.getOutputStream());
-			while(corriendo){
-				Date fecha = new Date();
-				SimpleDateFormat formato_fecha = new SimpleDateFormat("HH:mm:ss");
-				String hora=formato_fecha.format(fecha);
-				salida.writeUTF(hora);
-				Thread.sleep(1000);
-			}
-			socket_conexion.close();
+			Hilo hilo=new Hilo(socket_conexion);
+			hilo.start();
 			socket_servidor.close();
 		} catch (IOException e) {
 			e.printStackTrace();
